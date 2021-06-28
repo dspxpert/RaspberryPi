bc3738ecbb8313b8dde37354909256e45e9f5cd5
dspxpert <32154318+dspxpert@users.noreply.github.com>
2021-06-28 12:10:01 +0900

    Create LCD_test.py

---
+++ LCD_IO_Shield/LCD_test.py
@@ -0,0 +1,12 @@
+sudo pip3 install RPLCD
+
+from RPLCD.gpio import CharLCD
+lcd = CharLCD(pin_rs=8, pin_rw=None, pin_e=11, pins_data=[13, 15, 12, 16],
+              numbering_mode=GPIO.BOARD,
+              cols=16, rows=2, dotsize=8,
+              charmap='A02',
+              auto_linebreaks=True)
+
+lcd.clear()
+lcd.cursor_pos = (0, 0)
+lcd.write_string('Hello world')
