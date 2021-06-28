c009aa15c437a209f6344efd5e552dd5830faa1c
dspxpert <32154318+dspxpert@users.noreply.github.com>
2021-06-28 12:16:41 +0900

    Update LCD_test.py

--- LCD_IO_Shield/LCD_test.py
+++ LCD_IO_Shield/LCD_test.py
@@ -1,6 +1,10 @@
-sudo pip3 install RPLCD
+#!/usr/bin/python3
+#sudo pip3 install RPLCD
 
+import RPi.GPIO as GPIO
 from RPLCD.gpio import CharLCD
+
+GPIO.setwarnings(False)
 lcd = CharLCD(pin_rs=8, pin_rw=None, pin_e=11, pins_data=[13, 15, 12, 16],
               numbering_mode=GPIO.BOARD,
               cols=16, rows=2, dotsize=8,
